148a4bfaefef0c9fd39c56689a69dd3cf7870a6f
babybunn <wpakasame@gmail.com>
2024-11-05 17:19:32 +0700

    add gitignore

--- dist/errors/errors.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class DecodeError extends Error {
-    constructor(message: string);
-}
-export declare class SchemaError extends Error {
-    constructor(message: string);
-}
-export declare class EncodeError extends Error {
-    constructor(message: string);
-}
