626222bd4c2444308d98343546ed0aa90ffd9578
babybunn <wpakasame@gmail.com>
2024-11-05 17:03:05 +0700

    initial commit

---
+++ dist/errors/errors.d.ts
@@ -0,0 +1,9 @@
+export declare class DecodeError extends Error {
+    constructor(message: string);
+}
+export declare class SchemaError extends Error {
+    constructor(message: string);
+}
+export declare class EncodeError extends Error {
+    constructor(message: string);
+}
